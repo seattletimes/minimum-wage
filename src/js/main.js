935af34d0d3da6be46a3e6970c5fbeca16f74e8e
Audrey Carlsen <acarlsen@seattletimes.com>
2015-05-26 15:50:51 -0700

    adds in the flip directive

--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,22 @@ require("angular");
 
 var app = angular.module("minimum-wage", []);
 
+require("./flip")(app); //mix in our directive
+
 app.controller("CityController", ["$scope", function($scope) {
   $scope.when = "";
   $scope.cities = cityData;
-  
+
   $scope.sortCities = function() {
     $scope.cities.sort(function(a,b) {
       return (a["percent" + $scope.when] * 1) - (b["percent" + $scope.when] * 1);
     });
   };
   $scope.setWhen = function(when) {
-    $scope.when = when;
-    $scope.sortCities();
+    if ($scope.when !== when) {
+      $scope.when = when;
+      $scope.sortCities();
+    }
   };
 
   $scope.sortCities();
